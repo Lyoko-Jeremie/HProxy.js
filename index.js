ade1e4c91ad8b41d9de602c7a91342702987c160
Jeremie <lucheng989898@gmail.com>
2016-08-21 16:31:56 +0800

    add log

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+
 var express = require('express');
 var request = require('request');
 var url = require('url');
 var app = express();
 
+console.log("HProxy Initing.......");
+
 app.all('*', function (req, res) {
   var originUrl = req.header('origin-url');
 
-  console.log('proxy: ' + originUrl);
+  console.log('HProxy proxy: ' + originUrl);
   
   if (!originUrl) {
     res.end('H');
@@ -21,6 +24,9 @@ app.all('*', function (req, res) {
     url: originUrl,
     headers: headers
   }).pipe(res);
-})
+});
 
 app.listen(process.env.PORT || 5000);
+
+console.log("HProxy Inited!");
+console.log("HProxy Running....");
