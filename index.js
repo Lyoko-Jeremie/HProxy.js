7091e4bd886f16be0b387d23ba09207538cd8b92
Jeremie <lucheng989898@gmail.com>
2016-08-21 17:09:10 +0800

    fix commander

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var request = require('request');
-var program = require('commander');
+var commander = require('commander');
 var url = require('url');
 var app = express();
 
 console.log("HProxy Initing.......");
 
-program
-    .version('0.1.0')
-    .options('-p, --port <n>', 'listen port', 5000)
-    .parse(process.argv);
+commander.version('0.1.0');
+commander.option('-p, --port <n>', 'listen port', parseInt);
+commander.parse(process.argv);
 
 app.all('*', function (req, res) {
     var originUrl = req.header('origin-url');
@@ -31,8 +30,8 @@ app.all('*', function (req, res) {
     }).pipe(res);
 });
 
-console.log('HProxy will listen in %j port.', program.port || process.env.PORT || 5000);
-app.listen(program.port || process.env.PORT || 5000);
+console.log('HProxy will listen in %j port.', (commander.port || process.env.PORT || 5000));
+app.listen(commander.port || process.env.PORT || 5000);
 
 console.log("HProxy Inited!");
 console.log("HProxy Running....");
