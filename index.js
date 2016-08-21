23e5ce1a8b9c342e0fc0161e0182ac7e553d4b40
Jeremie <lucheng989898@gmail.com>
2016-08-21 16:49:30 +0800

    commader

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-
 var express = require('express');
 var request = require('request');
+var program = require('commander');
 var url = require('url');
 var app = express();
 
 console.log("HProxy Initing.......");
 
+program
+    .version('0.1.0')
+    .options('-p, --port <n>', 'listen port', 5000)
+    .parse(process.argv);
+
 app.all('*', function (req, res) {
-  var originUrl = req.header('origin-url');
+    var originUrl = req.header('origin-url');
 
-  console.log('HProxy proxy: ' + originUrl);
-  
-  if (!originUrl) {
-    res.end('H');
-    return;
-  }
+    console.log('HProxy proxy: ' + originUrl);
 
-  var target = url.parse(originUrl);
-  var headers = req.headers;
-  headers.host = target.host;
-  request({
-    method: req.method,
-    url: originUrl,
-    headers: headers
-  }).pipe(res);
+    if (!originUrl) {
+        res.end('H');
+        return;
+    }
+
+    var target = url.parse(originUrl);
+    var headers = req.headers;
+    headers.host = target.host;
+    request({
+        method: req.method,
+        url: originUrl,
+        headers: headers
+    }).pipe(res);
 });
 
-app.listen(process.env.PORT || 5000);
+console.log('HProxy will listen in %j port.', program.port || process.env.PORT || 5000);
+app.listen(program.port || process.env.PORT || 5000);
 
 console.log("HProxy Inited!");
 console.log("HProxy Running....");
